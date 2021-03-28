c134330e0899f7a938cac908f6cc41475d4091a5
Ilbien Paul <paulilb@students.zhaw.ch>
2021-03-28 18:55:32 +0200

    Added CheckRequestCount and write to json

--- auswertung/follow_like_search/helpers/twitter_api/getFollowersAPI.js
+++ auswertung/follow_like_search/helpers/twitter_api/getFollowersAPI.js
@@ -1,6 +1,21 @@
+import { delay } from "../other/delay";
+
+//15 requests in 15 minutes
 const fetch = require("node-fetch")
+const fs = require("fs")
+const requestCountJSON = require("../../followerRequestCount.json")
 require('dotenv').config()
 var bearerToken = process.env.bearerToken
+const MAX_REQUESTS = 15 //The twitter API only allows 15 requests in 15 minutes
+const FIFTEEN_MINUTES = 905000 //Fifteen minutes and 5 seconds in milliseconds
+
+if(!requestCountJSON.lastRequestTime){
+    var count = 0
+} else if((Date.now() - requestCountJSON.lastRequestTime) >= FIFTEEN_MINUTES) {
+    var count = 0
+} else {
+    var count = requestCountJSON.counter
+}
 
 export default async function getFollowers(userId) {
 
@@ -12,8 +27,9 @@ export default async function getFollowers(userId) {
     headers: myHeaders,
     redirect: 'follow'
     };
-    
+
     var fetchUrl = "https://api.twitter.com/2/users/" + userId.toString() +"/followers?max_results=1000"
+    await checkRequestCount()
     var resultJSON = await (await fetch(fetchUrl, requestOptions)).json()
     var followers = []
     
@@ -21,6 +37,7 @@ export default async function getFollowers(userId) {
 
     while(resultJSON.meta.next_token) {
         var newFetchUrl = fetchUrl + "&pagination_token=" + resultJSON.meta.next_token.toString()
+        await checkRequestCount()
         resultJSON = await (await fetch(newFetchUrl, requestOptions)).json()
         followers = addData(resultJSON.data, followers)
     }
@@ -34,4 +51,27 @@ function addData(data, list) {
         list.push(dataSetObject)
     })
     return list
+}
+
+async function checkRequestCount() {
+    count++ //one request was made
+
+    await writeToJSON(count)
+
+    if(count > MAX_REQUESTS) {
+        console.log("Maximum number of FOLLOWER REQUESTS has been reached!")
+        console.log("Please wait for 15 minutes...")
+        await delay(FIFTEEN_MINUTES)
+        count = 0
+    }
+}
+
+async function writeToJSON(number) {
+    var counted = {name: "followerRequests", counter: number, lastRequestTime: Date.now()}
+    var data = JSON.stringify(counted)
+    fs.writeFileSync('./followerRequestCount.json', data, err => {
+        if(err){
+            console.log(err)
+        }
+    })
 }
--- auswertung/follow_like_search/helpers/twitter_api/getFollowsAPI.js
+++ auswertung/follow_like_search/helpers/twitter_api/getFollowsAPI.js
@@ -1,6 +1,21 @@
+import { delay } from "../other/delay";
+
+//15 requests in 15 minutes
 const fetch = require("node-fetch")
+const fs = require("fs")
+const requestCountJSON = require("../../followsRequestCount.json")
 require('dotenv').config()
 var bearerToken = process.env.bearerToken
+const MAX_REQUESTS = 15 //The twitter API only allows 15 requests in 15 minutes
+const FIFTEEN_MINUTES = 905000 //Fifteen minutes and 5 seconds in milliseconds
+
+if(!requestCountJSON.lastRequestTime){
+    var count = 0
+} else if((Date.now() - requestCountJSON.lastRequestTime) >= FIFTEEN_MINUTES) {
+    var count = 0
+} else {
+    var count = requestCountJSON.counter
+}
 
 export default async function getFollows(userId) {
 
@@ -14,6 +29,7 @@ export default async function getFollows(userId) {
     };
 
     var fetchUrl = "https://api.twitter.com/2/users/" + userId.toString() +"/following?max_results=1000"
+    await checkRequestCount()
     var resultJSON = await (await fetch(fetchUrl, requestOptions)).json()
     var followedUsers = []
     
@@ -21,6 +37,7 @@ export default async function getFollows(userId) {
     
     while(resultJSON.meta.next_token) {
         var newFetchUrl = fetchUrl + "&pagination_token=" + resultJSON.meta.next_token.toString()
+        await checkRequestCount()
         resultJSON = await (await fetch(newFetchUrl, requestOptions)).json()
         followedUsers = addData(resultJSON.data, followedUsers)
     }
@@ -34,4 +51,27 @@ function addData(data, list) {
         list.push(dataSetObject)
     })
     return list
+}
+
+async function checkRequestCount() {
+    count++ //one request was made
+
+    await writeToJSON(count)
+
+    if(count > MAX_REQUESTS) {
+        console.log("Maximum number of FOLLOWING REQUESTS has been reached!")
+        console.log("Please wait for 15 minutes...")
+        await delay(FIFTEEN_MINUTES)
+        count = 0
+    }
+}
+
+async function writeToJSON(number) {
+    var counted = {name: "followsRequests", counter: number, lastRequestTime: Date.now()}
+    var data = JSON.stringify(counted)
+    fs.writeFileSync('./followsRequestCount.json', data, err => {
+        if(err){
+            console.log(err)
+        }
+    })
 }
