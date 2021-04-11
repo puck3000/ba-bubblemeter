1b4e285bd27b6c11d604cf3daedc868d520e0dfe
Ilbien Paul <paulilb@students.zhaw.ch>
2021-04-11 22:10:40 +0200

    Added try/catch sp that the program doesn't crash when no post is found

--- auswertung/follow_like_search/helpers/scraping/likeTweetFromOtherUser.js
+++ auswertung/follow_like_search/helpers/scraping/likeTweetFromOtherUser.js
@@ -1,9 +1,16 @@
+import { delay } from "../other/delay"
 
 
 export default async function likeTweetFromOtherUser(twitterer, tweetID, page) {
   await page.goto(`https://twitter.com/${twitterer}/status/${tweetID}`)
-  await page.waitForSelector('div[data-testid="like"]')
-  await page.click('div[data-testid="like"]')
+  try {
+    await page.waitForSelector('div[data-testid="like"]')
+    await page.click('div[data-testid="like"]')
+  }
+  catch(err) {
+    console.log("There was an error!")
+  }
+  await delay(2000)
   console.log(`Liked ${tweetID}!`)
 }
 
