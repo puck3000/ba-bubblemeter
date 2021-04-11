39fdf9bd07552eccf8ac7b9cc8ed49a35ee07e2c
Ilbien Paul <paulilb@students.zhaw.ch>
2021-04-11 22:14:22 +0200

    Added max amount of 200 likes and mechanism to return always the next 200 oldest

--- auswertung/follow_like_search/helpers/twitter_api/getLikesAPI.js
+++ auswertung/follow_like_search/helpers/twitter_api/getLikesAPI.js
@@ -14,7 +14,18 @@ export default async function getLikes(userId) {
     redirect: 'follow'
     };
 
-    var fetchUrl = "https://api.twitter.com/1.1/favorites/list.json?user_id=" + userId.toString()
-    var result = (await fetch(fetchUrl, requestOptions)).json()
+    var fetchUrl = "https://api.twitter.com/1.1/favorites/list.json?user_id=" + userId.toString() + "&count=200"
+    var result = await (await fetch(fetchUrl, requestOptions)).json()
+
+    var newOldest = 0
+    var lastOldest = 1
+    while(lastOldest !== newOldest){
+        lastOldest = result[result.length - 1].id_str.toString()
+        var newFetchUrl = "https://api.twitter.com/1.1/favorites/list.json?user_id=" + userId.toString() + "&count=200&max_id=" + result[result.length - 1].id_str.toString()
+        var newResult = await (await fetch(newFetchUrl, requestOptions)).json()
+        result = result.concat(newResult)
+        newOldest = result[result.length - 1].id_str.toString()
+    }
+
     return result
 }
