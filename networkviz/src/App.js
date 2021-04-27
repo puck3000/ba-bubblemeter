23be4dc50d3fbbde73c17c8fe2f346b8231632e5
Daniel Lerch <daniel@daniellerch.ch>
2021-04-27 08:12:33 +0200

    removes console.log

--- networkviz/src/App.js
+++ networkviz/src/App.js
@@ -6,14 +6,13 @@ import { default as data } from './data.json'
 function App() {
   const topten = data.body.top_ten_most_influential.data
   const politicians = data.body.politicians_in_network.data
-  console.log(topten)
 
   return (
     <div className='App min-h-screen flex flex-col p-2'>
       <header>
         <h1 className='text-4xl'>Polit-o-Meter</h1>
         <h2 className='text-sm mb-6'>
-          <span className='font-mono'>Stand:</span> 16. April 2021
+          <span className='font-mono'>Stand:</span> 26. April 2021
         </h2>
       </header>
       <main className='flex-1'>
