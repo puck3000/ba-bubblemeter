faad4d0e3f72d9b434ab63ac8f3ba49abee8fa91
Daniel Lerch <daniel@daniellerch.ch>
2021-04-29 17:58:34 +0200

    datada

--- nrKandidaten/scraper.js
+++ nrKandidaten/scraper.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer')
+const fs = require('fs')
 
 ;(async function scrape() {
   const browser = await puppeteer.launch({
@@ -8,84 +9,61 @@ const puppeteer = require('puppeteer')
 
   const page = await browser.newPage()
 
-  await page.setViewport({
-    width: 1280,
-    height: 980,
-    deviceScaleFactor: 1,
-  })
+  let politicians = []
 
-  // might be helpful for not getting tossed out that quickly...
-  page.setUserAgent(
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299'
-  )
+  const nrOfLoops = 3893
+  let i = 0
 
-  // open smartmap page
-  await page.goto(
-    'https://www.smartvote.ch/de/group/2/election/19_ch_nr/smartmap',
-    { waitUntil: 'networkidle2' }
-  )
+  for (i; i < nrOfLoops; i++) {
+    try {
+      // open smartmap page
+      await page.goto(
+        'https://www.smartvote.ch/de/group/2/election/19_ch_nr/smartmap',
+        { waitUntil: 'networkidle2' }
+      )
 
-  //  click on "anzeigen"
-  await page.waitForSelector('button[type="submit"]')
-  await page.click('button[type="submit"]')
+      //  click on "anzeigen"
+      await page.waitForSelector('button[type="submit"]')
+      await page.click('button[type="submit"]')
 
-  //   wait for svg-circles to load
-  await page.waitForSelector('.smartmap-position')
+      //   wait for svg-circles to load
+      await page.waitForSelector('.smartmap-position')
 
-  //   get array of circles
+      //   get the circle
+      const circles = await page.$$('.smartmap-position')
+      const circle = circles[i]
 
-  const circles = await page.$$('.smartmap-position')
-  console.log('lenght', circles.length)
+      // get the coordinates
+      let koordinaten = await circle.evaluate((cir) => {
+        let x = cir.getAttribute('cx')
+        let y = cir.getAttribute('cy')
+        let coords = {
+          x,
+          y,
+        }
+        return coords
+      }, circle)
 
-  let data = []
+      // get the link
+      let url
+      await circles[i].click()
+      await page.waitForSelector('.sv-candidate-page')
+      url = await page.url()
+      let smartMapId = url.substring(url.lastIndexOf('/') + 1)
 
-  for (circle of circles.slice([0], [10])) {
-    // gettin the coordinates
-    let koordinaten = await circle.evaluate((cir) => {
-      let x = cir.getAttribute('cx')
-      let y = cir.getAttribute('cy')
-      let coords = {
-        x,
-        y,
-      }
-      return coords
-    }, circle)
-
-    // TODO: getting the links
-
-    let candidate = {
-      koordinaten: koordinaten,
+      // push data
+      console.log({ smartMapId, koordinaten })
+      politicians.push({
+        smartMapId,
+        koordinaten,
+      })
+    } catch (error) {
+      console.log(error)
     }
-
-    data.push(candidate)
   }
+  console.log(politicians)
 
-  console.log(data)
-
-  // for (let i = 0; i < 5; i++) {
-  //   let url
-  //   try {
-  //     await circles[i].click()
-  //     url = await page.url()
-  //     console.log(url)
-  //     await page.goBack()
-  //     url = await page.url()
-  //     console.log(url)
-  //   } catch (error) {
-  //     console.log(error)
-  //   }
-  // }
-
-  // now by hand
-
-  // let url
-  // await circles[0].click({ waitUntil: 'networkidle0' })
-  // url = await page.url()
-  // console.log(url)
-  // await page.goBack({ waitUntil: 'networkidle0' })
-  // url = await page.url()
-  // console.log(url)
-  // await circles[1].click({ waitUntil: 'networkidle0' })
-  // url = await page.url()
-  // console.log(url)
+  // write file
+  const data = JSON.stringify(politicians)
+  fs.writeFileSync('politikerMitTwitterNew.json', data)
 })()
